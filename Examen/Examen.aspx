1c88c2307f63d83abbdc063f3de599d803b83752
Mateo <matecruzh@gmail.com>
2022-05-19 19:13:13 -0500

    SubiendoCodigoFinal

---
+++ Examen/Examen.aspx
@@ -0,0 +1,128 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Examen.aspx.cs" Inherits="Examen.Examen" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <script>
+        function soloNumeros(e) {
+            var key = window.Event ? e.which : e.keyCode
+            return (key >= 48 && key <= 57)
+        }
+
+        function consultaDom() {
+            document.getElementById("<%=btn.ClientID%>").click();
+            return false;
+        }
+
+        function validaCampos() {
+            if (document.getElementById("<%=txtEstado.ClientID%>").value == "" ||
+                document.getElementById("<%=txtMunicipio.ClientID%>").value == "" ||
+                document.getElementById("<%=txtColonia.ClientID%>").value == "") {
+                alert("Faltan datos por capturar.");
+            }else{
+                alert("Los datos están completos.");
+            }
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="dvVehiculo">
+        <table style="width: 100%">
+            <tr>
+                <td colspan="2">
+                    <asp:Label runat="server" ID="lblTitulo1"><h2>Vehículo</h2></asp:Label>
+                </td>
+            </tr>
+            <tr><td colspan="2"><hr /></td></tr>
+            <tr>
+                <td>
+                    <asp:Label runat="server" Text="Marca:"></asp:Label>
+                </td>
+                <td>
+                    <asp:DropDownList runat="server" ID="ddlMarca" AutoPostBack="True" OnSelectedIndexChanged="ddlMarca_SelectedIndexChanged"></asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label runat="server" Text="SubMarca:"></asp:Label>
+                </td>
+                <td>
+                    <asp:DropDownList runat="server" ID="ddlSubmarca" AutoPostBack="True" OnSelectedIndexChanged="ddlSubmarca_SelectedIndexChanged"></asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label runat="server" Text="Modelo:"></asp:Label>
+                </td>
+                <td>
+                    <asp:DropDownList runat="server" ID="ddlModelo" AutoPostBack="True" OnSelectedIndexChanged="ddlModelo_SelectedIndexChanged"></asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label runat="server" Text="Descripción:"></asp:Label>
+                </td>
+                <td>
+                    <asp:DropDownList runat="server" ID="ddlDesc"></asp:DropDownList>
+                </td>
+            </tr>
+        </table>
+    </div>
+        <br /><br />
+    <hr />
+    <div id="dvDomicilio">
+        <table style="width: 100%">
+            <tr>
+                <td colspan="2">
+                    <asp:Label runat="server" ID="Label1"><h2>Domicilio</h2></asp:Label>
+                </td>
+            </tr>
+            <tr><td colspan="2"><hr /></td></tr>
+            <tr>
+                <td>
+                    <asp:Label runat="server" Text="Código Postal:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox runat="server" ID="txtCP" MaxLength="5" onKeyPress="return soloNumeros(event);" onblur="consultaDom();"> </asp:TextBox>
+                    <asp:Button ID="btn" runat="server" OnClick="btn_Click" style="display:none;"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label runat="server" Text="Estado:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox runat="server" ID="txtEstado"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label runat="server" Text="Municipio:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox runat="server" ID="txtMunicipio" ></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label runat="server" Text="Colonia:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox runat="server" ID="txtColonia" ></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="2">
+                    <input type="button" id="btnValida" onclick="validaCampos()" value="Validar"/>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Examen/Examen.aspx.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
+using System.Dynamic;
+using Newtonsoft.Json.Converters;
+using System.Linq;
+using System.Web.Services;
+
+namespace Examen
+{
+    public partial class Examen : System.Web.UI.Page
+    {
+        private string srtParameters;
+        private string url = "https://apitestcotizamatico.azurewebsites.net/api/catalogos";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                llenaComboInicial();
+        }
+
+        private void llenaComboInicial()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            ddlSubmarca.Items.Clear();
+            ddlModelo.Items.Clear();
+            ddlDesc.Items.Clear();
+
+            var obj = new
+            {
+                NombreCatalogo = "Marca",
+                Filtro = "1",
+                IdAplication = "2"
+            };
+
+            srtParameters = JsonConvert.SerializeObject(obj);
+            HttpContent content = new StringContent(srtParameters, Encoding.UTF8, "application/json");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            var response = client.PostAsync(url, content).Result;
+            dynamic data = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result.ToString());
+            var list = JsonConvert.DeserializeObject<List<Marcas>>(data["CatalogoJsonString"].ToString());
+            ListItem i = null;
+            i = new ListItem("Selecciona una Marca", "0");
+            ddlMarca.Items.Add(i);
+
+            foreach (var item in list)
+            {
+                i = new ListItem(item.sMarca, Convert.ToString(item.iIdMarca));
+                ddlMarca.Items.Add(i);
+            }
+        }
+
+        public void ddlMarca_SelectedIndexChanged(Object sender, EventArgs e)
+        {
+            if (ddlMarca.SelectedValue.ToString() != null)
+            {
+                ddlSubmarca.Items.Clear();
+                ddlModelo.Items.Clear();
+                ddlDesc.Items.Clear();
+
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var obj = new
+                {
+                    NombreCatalogo = "Submarca",
+                    Filtro = ddlMarca.SelectedValue,
+                    IdAplication = "2"
+                };
+
+                srtParameters = JsonConvert.SerializeObject(obj);
+                HttpContent content = new StringContent(srtParameters, Encoding.UTF8, "application/json");
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                var response = client.PostAsync(url, content).Result;
+                dynamic data = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result.ToString());
+                var list = JsonConvert.DeserializeObject<List<SubMarcas>>(data["CatalogoJsonString"].ToString());
+                ListItem i = null;
+                i = new ListItem("Selecciona una Submarca", "0");
+                ddlSubmarca.Items.Add(i);
+
+                foreach (var item in list)
+                {
+                    i = new ListItem(item.sSubMarca, Convert.ToString(item.iIdSubMarca));
+                    ddlSubmarca.Items.Add(i);
+                }
+            }
+        }
+
+        public void ddlSubmarca_SelectedIndexChanged(Object sender, EventArgs e)
+        {
+            if (ddlSubmarca.SelectedValue.ToString() != null)
+            {
+                ddlModelo.Items.Clear();
+                ddlDesc.Items.Clear();
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var obj = new
+                {
+                    NombreCatalogo = "Modelo",
+                    Filtro = ddlSubmarca.SelectedValue,
+                    IdAplication = "2"
+                };
+
+                srtParameters = JsonConvert.SerializeObject(obj);
+                HttpContent content = new StringContent(srtParameters, Encoding.UTF8, "application/json");
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                var response = client.PostAsync(url, content).Result;
+                dynamic data = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result.ToString());
+                var list = JsonConvert.DeserializeObject<List<Modelo>>(data["CatalogoJsonString"].ToString());
+                ListItem i = null;
+                i = new ListItem("Selecciona un Modelo", "0");
+                ddlModelo.Items.Add(i);
+
+                foreach (var item in list)
+                {
+                    i = new ListItem(item.sModelo, Convert.ToString(item.iIdModelo));
+                    ddlModelo.Items.Add(i);
+                }
+            }
+        }
+
+        public void ddlModelo_SelectedIndexChanged(Object sender, EventArgs e)
+        {
+            if (ddlModelo.SelectedValue.ToString() != null)
+            {
+                ddlDesc.Items.Clear();
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var obj = new
+                {
+                    NombreCatalogo = "DescripcionModelo",
+                    Filtro = ddlModelo.SelectedValue,
+                    IdAplication = "2"
+                };
+
+                srtParameters = JsonConvert.SerializeObject(obj);
+                HttpContent content = new StringContent(srtParameters, Encoding.UTF8, "application/json");
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                var response = client.PostAsync(url, content).Result;
+                dynamic data = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result.ToString());
+                var list = JsonConvert.DeserializeObject<List<DescModelo>>(data["CatalogoJsonString"].ToString());
+                ListItem i = null;
+                i = new ListItem("Selecciona un Modelo", "0");
+                ddlDesc.Items.Add(i);
+
+                foreach (var item in list)
+                {
+                    i = new ListItem(item.sDescripcion, Convert.ToString(item.iIdDescripcionModelo));
+                    ddlDesc.Items.Add(i);
+                }
+            }
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            txtEstado.Text = "";
+            txtMunicipio.Text = "";
+            txtColonia.Text = "";
+
+            var obj = new
+            {
+                NombreCatalogo = "Sepomex",
+                Filtro = txtCP.Text,
+                IdAplication = "2"
+            };
+
+            srtParameters = JsonConvert.SerializeObject(obj);
+            HttpContent content = new StringContent(srtParameters, Encoding.UTF8, "application/json");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = client.PostAsync(url, content).Result;
+            dynamic data = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result.ToString());
+            dynamic data2 = JsonConvert.DeserializeObject(data["CatalogoJsonString"].Value);
+
+            txtEstado.Text = data2[0]["Municipio"]["Estado"]["sEstado"];
+            txtMunicipio.Text = data2[0]["Municipio"]["sMunicipio"];
+            txtColonia.Text = data2[0]["Ubicacion"][0]["sUbicacion"];
+        }
+    }
+
+    public class Marcas
+    {
+        public int iIdMarca { get; set; }
+        public string sMarca { get; set; }
+    }
+
+    public class SubMarcas
+    {
+        public int iIdSubMarca { get; set; }
+        public string sSubMarca { get; set; }
+    }
+
+    public class Modelo
+    {
+        public int iIdModelo { get; set; }
+        public string sModelo{ get; set; }
+    }
+
+    public class DescModelo
+    {
+        public int iIdDescripcionModelo { get; set; }
+        public string sDescripcion { get; set; }
+    }
+    
+}
